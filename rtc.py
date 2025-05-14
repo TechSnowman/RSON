c34b6b3c87f69108e234372b5c0668b699a86650
CurtisJaekl <80610132+GM-Chess@users.noreply.github.com>
2025-05-14 13:14:12 -0400

    changed some syntax errors

--- rtc.py
+++ rtc.py
@@ -1,7 +1,7 @@
-import SDL_DS3231
+import DS32131 as rtc
 from datetime import datetime
 
-rtc_device = SDL_DS3231.SDL_DS3231(twi=1, addr=0x68, at24c32_addr=0x57)
+rtc_device = rtc.SDL_DS3231(twi=1, addr=0x68, at24c32_addr=0x57)
 
 rtc_time_utc = rtc_device.read_datetime()
 print(f"time is {rtc_time_utc}")
